b00e3bcadd71ba0495cabeac898b4bf2dac55ffa
sudhandeveloper <gmsudhan3@gmail.com>
2024-03-08 18:01:54 +0530

    new

--- my-app/src/component/navbar.jsx
+++ my-app/src/component/navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { FaBars } from "react-icons/fa6";
 import { AiOutlineClose } from "react-icons/ai";
 import Pageone from "../pages/page-one";
-import PageTwo from "../pages/pagetwo"; 
+import PageTwo from "../pages/pagetwo";
 const Navbar = () => {
   const [drawer, setDrawer] = useState("false");
 
@@ -16,129 +16,121 @@ const Navbar = () => {
   return (
     <>
       {" "}
-      <div className="bg-black p-5">
-        <div className="">
-          <div onClick={clickerhandler} className="text-white ">
-            {drawer ? (
-              <FaBars className="md:hidden" />
-            ) : (
-              <AiOutlineClose className="md:hidden" />
-            )}
+      <div className="p-5 bg-black">
+        <div className="relative">
+          <div
+            onClick={clickerhandler}
+            className="absolute z-30 text-white -top-2"
+          >
+            {drawer ? <FaBars className="md:hidden" /> : ""}
           </div>
         </div>
-      </div>{" "}
-      <div className="flex relative">
-      <div
-        className={`bg-red-400  max-md:absolute max-md:top-0  ${
-          drawer ? "max-md:hidden" : ""
-        }`}
-      >
-        <aside className="w-full md:h-[650px] p-6 sm:w-60 dark:bg-gray-900 md:overflow-auto dark:text-gray-100 ">
-          <nav className="space-y-8 text-sm">
-            <div className="space-y-2">
-              <h2 className="text-sm font-semibold tracki uppercase dark:text-gray-400">
-                Getting Started
-              </h2>
-              <div className="flex flex-col space-y-1 ">
-                <div className="text-2xl text-red-400 " onClick={() => setMenu("Pageone")}>
-                  Page ONE
+      </div>
+      <div className="relative flex">
+        <div
+          className={`bg-red-400  max-md:fixed max-md:top-0  ${
+            drawer ? "max-md:hidden" : ""
+          }`}
+        >
+          
+          <aside className="w-full md:h-[649px] p-6 sm:w-60 dark:bg-gray-900 overflow-auto dark:text-gray-100 ">
+            <nav className="space-y-8 text-sm">
+              <div onClick={clickerhandler} className="text-white ">
+                {drawer ? "" : <AiOutlineClose className="md:hidden" />}
+              </div>
+              
+              <div className="space-y-2">
+                <h2 className="text-sm font-semibold uppercase tracki dark:text-gray-400">
+                  Getting Started
+                </h2>
+                <div className="flex flex-col space-y-1 ">
+                  <div
+                    className="text-2xl text-red-400 cursor-pointer"
+                    onClick={() => setMenu("Pageone")}
+                  >
+                    Page ONE
+                  </div>
+                  <div
+                    className="cursor-pointer hover:text-red-400"
+                    onClick={() => setMenu("PageTwo")}
+                  >
+                    Page TWO
+                  </div>
+                  <div className="hover:text-red-400">Page Three</div>
+                  <a rel="noopener noreferrer" href="#">
+                    Appearance
+                  </a>
+                  <a rel="noopener noreferrer" href="#">
+                    Mamba UI
+                  </a>
                 </div>
-                <div className="hover:text-red-400"> onClick={() => setMenu("PageTwo")}
-                  Page TWO
+              </div>
+              <div className="space-y-2">
+                <h2 className="text-sm font-semibold uppercase tracki dark:text-gray-400">
+                  Dashboard
+                </h2>
+                <div className="flex flex-col space-y-1">
+                  <a rel="noopener noreferrer" href="#">
+                    Header
+                  </a>
+                  <a rel="noopener noreferrer" href="#">
+                    Drawer
+                  </a>
+                  <a rel="noopener noreferrer" href="#">
+                    Page Title
+                  </a>
+                  <a rel="noopener noreferrer" href="#">
+                    Menus
+                  </a>
+                  <a rel="noopener noreferrer" href="#">
+                    Sidebar
+                  </a>
+                  <a rel="noopener noreferrer" href="#">
+                    Footer
+                  </a>
                 </div>
-                <div className="hover:text-red-400">
-                  Page Three
+              </div>
+              <div className="space-y-2">
+                <h2 className="text-sm font-semibold uppercase tracki dark:text-gray-400">
+                  Pages
+                </h2>
+                <div className="flex flex-col space-y-1">
+                  <a rel="noopener noreferrer" href="#">
+                    Homepage
+                  </a>
+                  <a rel="noopener noreferrer" href="#">
+                    Users
+                  </a>
+                  <a rel="noopener noreferrer" href="#">
+                    Tools
+                  </a>
+                  <a rel="noopener noreferrer" href="#">
+                    Settings
+                  </a>
                 </div>
-                <a rel="noopener noreferrer" href="#">
-                  Appearance
-                </a>
-                <a rel="noopener noreferrer" href="#">
-                  Mamba UI
-                </a>
               </div>
-            </div>
-            <div className="space-y-2">
-              <h2 className="text-sm font-semibold tracki uppercase dark:text-gray-400">
-                Dashboard
-              </h2>
-              <div className="flex flex-col space-y-1">
-                <a rel="noopener noreferrer" href="#">
-                  Header
-                </a>
-                <a rel="noopener noreferrer" href="#">
-                  Drawer
-                </a>
-                <a rel="noopener noreferrer" href="#">
-                  Page Title
-                </a>
-                <a rel="noopener noreferrer" href="#">
-                  Menus
-                </a>
-                <a rel="noopener noreferrer" href="#">
-                  Sidebar
-                </a>
-                <a rel="noopener noreferrer" href="#">
-                  Footer
-                </a>
+              <div className="space-y-2">
+                <h2 className="text-sm font-semibold uppercase tracki dark:text-gray-400">
+                  Misc
+                </h2>
+                <div className="flex flex-col space-y-1">
+                  <a rel="noopener noreferrer" href="#">
+                    Tutorials
+                  </a>
+                  <a rel="noopener noreferrer" href="#">
+                    Changelog
+                  </a>
+                </div>
               </div>
-            </div>
-            <div className="space-y-2">
-              <h2 className="text-sm font-semibold tracki uppercase dark:text-gray-400">
-                Pages
-              </h2>
-              <div className="flex flex-col space-y-1">
-                <a rel="noopener noreferrer" href="#">
-                  Homepage
-                </a>
-                <a rel="noopener noreferrer" href="#">
-                  Users
-                </a>
-                <a rel="noopener noreferrer" href="#">
-                  Tools
-                </a>
-                <a rel="noopener noreferrer" href="#">
-                  Settings
-                </a>
-              </div>
-            </div>
-            <div className="space-y-2">
-              <h2 className="text-sm font-semibold tracki uppercase dark:text-gray-400">
-                Misc
-              </h2>
-              <div className="flex flex-col space-y-1">
-                <a rel="noopener noreferrer" href="#">
-                  Tutorials
-                </a>
-                <a rel="noopener noreferrer" href="#">
-                  Changelog
-                </a>
-              </div>
-            </div>
-            <div className="space-y-2">
-              <h2 className="text-sm font-semibold tracki uppercase dark:text-gray-400">
-                Misc
-              </h2>
-              <div className="flex flex-col space-y-1">
-                <a rel="noopener noreferrer" href="#">
-                  Tutorials
-                </a>
-                <a rel="noopener noreferrer" href="#">
-                  Changelog
-                </a>
-              </div>
-            </div>
+            </nav>
+          </aside>
+        </div>
 
-            
-          </nav>
-        </aside>
+        <div className="bg-gray-100 w-full md:h-[650px] md:overflow-auto">
+          {menu === "Pageone" && <Pageone />}
+          {menu === "PageTwo" && <PageTwo />}
+        </div>
       </div>
-
-      <div className="bg-gray-100 w-full md:h-[650px] md:overflow-auto">
-      {menu === "Pageone" && <Pageone />}
-      {menu === "PageTwo" && <PageTwo />}
-      </div>
-      </div>
-    
     </>
   );
 };
--- my-app/src/pages/pagetwo.jsx
+++ my-app/src/pages/pagetwo.jsx
@@ -5,24 +5,24 @@ const PageTwo = () => {
     <div>
    <div className="flex flex-col items-center justify-center p-5 overflow-auto">
 	<div className="flex space-x-5">
-		<img alt="" className="w-12 h-12 rounded-full ri ri dark:bg-gray-500 ri ri" src="https://source.unsplash.com/40x40/?portrait?1" />
-		<img alt="" className="w-12 h-12 rounded-full ri ri dark:bg-gray-500 ri ri" src="https://source.unsplash.com/40x40/?portrait?2" />
-		<img alt="" className="w-12 h-12 rounded-full ri ri dark:bg-gray-500 ri ri" src="https://source.unsplash.com/40x40/?portrait?3" />
-		<img alt="" className="w-12 h-12 rounded-full ri ri dark:bg-gray-500 ri ri" src="https://source.unsplash.com/40x40/?portrait?4" />
-    <img alt="" className="w-12 h-12 rounded-full ri ri dark:bg-gray-500 ri ri" src="https://source.unsplash.com/40x40/?portrait?1" />
-		<img alt="" className="w-12 h-12 rounded-full ri ri dark:bg-gray-500 ri ri" src="https://source.unsplash.com/40x40/?portrait?2" />
-		<img alt="" className="w-12 h-12 rounded-full ri ri dark:bg-gray-500 ri ri" src="https://source.unsplash.com/40x40/?portrait?3" />
-		<img alt="" className="w-12 h-12 rounded-full ri ri dark:bg-gray-500 ri ri" src="https://source.unsplash.com/40x40/?portrait?4" />
-    <img alt="" className="w-12 h-12 rounded-full ri ri dark:bg-gray-500 ri ri" src="https://source.unsplash.com/40x40/?portrait?1" />
-		<img alt="" className="w-12 h-12 rounded-full ri ri dark:bg-gray-500 ri ri" src="https://source.unsplash.com/40x40/?portrait?2" />
-		<img alt="" className="w-12 h-12 rounded-full ri ri dark:bg-gray-500 ri ri" src="https://source.unsplash.com/40x40/?portrait?3" />
-		<img alt="" className="w-12 h-12 rounded-full ri ri dark:bg-gray-500 ri ri" src="https://source.unsplash.com/40x40/?portrait?4" />
-    <img alt="" className="w-12 h-12 rounded-full ri ri dark:bg-gray-500 ri ri" src="https://source.unsplash.com/40x40/?portrait?2" />
-		<img alt="" className="w-12 h-12 rounded-full ri ri dark:bg-gray-500 ri ri" src="https://source.unsplash.com/40x40/?portrait?3" />
-		<img alt="" className="w-12 h-12 rounded-full ri ri dark:bg-gray-500 ri ri" src="https://source.unsplash.com/40x40/?portrait?4" />
-    <img alt="" className="w-12 h-12 rounded-full ri ri dark:bg-gray-500 ri ri" src="https://source.unsplash.com/40x40/?portrait?2" />
-		<img alt="" className="w-12 h-12 rounded-full ri ri dark:bg-gray-500 ri ri" src="https://source.unsplash.com/40x40/?portrait?3" />
-		<img alt="" className="w-12 h-12 rounded-full ri ri dark:bg-gray-500 ri ri" src="https://source.unsplash.com/40x40/?portrait?4" />
+		<img alt="" className="w-12 h-12 rounded-full ri dark:bg-gray-500" src="https://source.unsplash.com/40x40/?portrait?1" />
+		<img alt="" className="w-12 h-12 rounded-full ri dark:bg-gray-500" src="https://source.unsplash.com/40x40/?portrait?2" />
+		<img alt="" className="w-12 h-12 rounded-full ri dark:bg-gray-500" src="https://source.unsplash.com/40x40/?portrait?3" />
+		<img alt="" className="w-12 h-12 rounded-full ri dark:bg-gray-500" src="https://source.unsplash.com/40x40/?portrait?4" />
+    <img alt="" className="w-12 h-12 rounded-full ri dark:bg-gray-500" src="https://source.unsplash.com/40x40/?portrait?1" />
+		<img alt="" className="w-12 h-12 rounded-full ri dark:bg-gray-500" src="https://source.unsplash.com/40x40/?portrait?2" />
+		<img alt="" className="w-12 h-12 rounded-full ri dark:bg-gray-500" src="https://source.unsplash.com/40x40/?portrait?3" />
+		<img alt="" className="w-12 h-12 rounded-full ri dark:bg-gray-500" src="https://source.unsplash.com/40x40/?portrait?4" />
+    <img alt="" className="w-12 h-12 rounded-full ri dark:bg-gray-500" src="https://source.unsplash.com/40x40/?portrait?1" />
+		<img alt="" className="w-12 h-12 rounded-full ri dark:bg-gray-500" src="https://source.unsplash.com/40x40/?portrait?2" />
+		<img alt="" className="w-12 h-12 rounded-full ri dark:bg-gray-500" src="https://source.unsplash.com/40x40/?portrait?3" />
+		<img alt="" className="w-12 h-12 rounded-full ri dark:bg-gray-500" src="https://source.unsplash.com/40x40/?portrait?4" />
+    <img alt="" className="w-12 h-12 rounded-full ri dark:bg-gray-500" src="https://source.unsplash.com/40x40/?portrait?2" />
+		<img alt="" className="w-12 h-12 rounded-full ri dark:bg-gray-500" src="https://source.unsplash.com/40x40/?portrait?3" />
+		<img alt="" className="w-12 h-12 rounded-full ri dark:bg-gray-500" src="https://source.unsplash.com/40x40/?portrait?4" />
+    <img alt="" className="w-12 h-12 rounded-full ri dark:bg-gray-500" src="https://source.unsplash.com/40x40/?portrait?2" />
+		<img alt="" className="w-12 h-12 rounded-full ri dark:bg-gray-500" src="https://source.unsplash.com/40x40/?portrait?3" />
+		<img alt="" className="w-12 h-12 rounded-full ri dark:bg-gray-500" src="https://source.unsplash.com/40x40/?portrait?4" />
 
 	</div>
 </div>
@@ -42,7 +42,7 @@ const PageTwo = () => {
 	</div>
 </section> */}
 
-<section className="py-6 sm:py-12  ">
+<section className="py-6 sm:py-12 ">
 	<div className="container p-6 mx-auto space-y-8">
 		<div className="space-y-2 text-center dark:text-black">
 			<h2 className="text-3xl font-bold">Partem reprimique an pro</h2>
@@ -55,7 +55,7 @@ const PageTwo = () => {
 				</a>
 				<div className="flex flex-col flex-1 p-6">
 					<a rel="noopener noreferrer" href="#" aria-label="Te nulla oportere reprimique his dolorum"></a>
-					<a rel="noopener noreferrer" href="#" className="text-xs tracki uppercase hover:underline dark:text-violet-400">Convenire</a>
+					<a rel="noopener noreferrer" href="#" className="text-xs uppercase tracki hover:underline dark:text-violet-400">Convenire</a>
 					<h3 className="flex-1 py-2 text-lg font-semibold leadi">Te nulla oportere reprimique his dolorum</h3>
 					<div className="flex flex-wrap justify-between pt-3 space-x-2 text-xs dark:text-gray-400">
 						<span>June 1, 2020</span>
@@ -69,7 +69,7 @@ const PageTwo = () => {
 				</a>
 				<div className="flex flex-col flex-1 p-6">
 					<a rel="noopener noreferrer" href="#" aria-label="Te nulla oportere reprimique his dolorum"></a>
-					<a rel="noopener noreferrer" href="#" className="text-xs tracki uppercase hover:underline dark:text-violet-400">Convenire</a>
+					<a rel="noopener noreferrer" href="#" className="text-xs uppercase tracki hover:underline dark:text-violet-400">Convenire</a>
 					<h3 className="flex-1 py-2 text-lg font-semibold leadi">Te nulla oportere reprimique his dolorum</h3>
 					<div className="flex flex-wrap justify-between pt-3 space-x-2 text-xs dark:text-gray-400">
 						<span>June 2, 2020</span>
@@ -83,7 +83,7 @@ const PageTwo = () => {
 				</a>
 				<div className="flex flex-col flex-1 p-6">
 					<a rel="noopener noreferrer" href="#" aria-label="Te nulla oportere reprimique his dolorum"></a>
-					<a rel="noopener noreferrer" href="#" className="text-xs tracki uppercase hover:underline dark:text-violet-400">Convenire</a>
+					<a rel="noopener noreferrer" href="#" className="text-xs uppercase tracki hover:underline dark:text-violet-400">Convenire</a>
 					<h3 className="flex-1 py-2 text-lg font-semibold leadi">Te nulla oportere reprimique his dolorum</h3>
 					<div className="flex flex-wrap justify-between pt-3 space-x-2 text-xs dark:text-gray-400">
 						<span>June 3, 2020</span>
@@ -91,64 +91,9 @@ const PageTwo = () => {
 					</div>
 				</div>
 			</article>
-			<article className="flex flex-col dark:bg-gray-900">
-				<a rel="noopener noreferrer" href="#" aria-label="Te nulla oportere reprimique his dolorum">
-					<img alt="" className="object-cover w-full h-52 dark:bg-gray-500" src="https://source.unsplash.com/200x200/?fashion?4" />
-				</a>
-				<div className="flex flex-col flex-1 p-6">
-					<a rel="noopener noreferrer" href="#" aria-label="Te nulla oportere reprimique his dolorum"></a>
-					<a rel="noopener noreferrer" href="#" className="text-xs tracki uppercase hover:underline dark:text-violet-400">Convenire</a>
-					<h3 className="flex-1 py-2 text-lg font-semibold leadi">Te nulla oportere reprimique his dolorum</h3>
-					<div className="flex flex-wrap justify-between pt-3 space-x-2 text-xs dark:text-gray-400">
-						<span>June 4, 2020</span>
-						<span>2.4K views</span>
-					</div>
-				</div>
-			</article>
-      <article className="flex flex-col dark:bg-gray-900">
-				<a rel="noopener noreferrer" href="#" aria-label="Te nulla oportere reprimique his dolorum">
-					<img alt="" className="object-cover w-full h-52 dark:bg-gray-500" src="https://source.unsplash.com/200x200/?fashion?4" />
-				</a>
-				<div className="flex flex-col flex-1 p-6">
-					<a rel="noopener noreferrer" href="#" aria-label="Te nulla oportere reprimique his dolorum"></a>
-					<a rel="noopener noreferrer" href="#" className="text-xs tracki uppercase hover:underline dark:text-violet-400">Convenire</a>
-					<h3 className="flex-1 py-2 text-lg font-semibold leadi">Te nulla oportere reprimique his dolorum</h3>
-					<div className="flex flex-wrap justify-between pt-3 space-x-2 text-xs dark:text-gray-400">
-						<span>June 4, 2020</span>
-						<span>2.4K views</span>
-					</div>
-				</div>
-			</article>
 
-      <article className="flex flex-col dark:bg-gray-900">
-				<a rel="noopener noreferrer" href="#" aria-label="Te nulla oportere reprimique his dolorum">
-					<img alt="" className="object-cover w-full h-52 dark:bg-gray-500" src="https://source.unsplash.com/200x200/?fashion?4" />
-				</a>
-				<div className="flex flex-col flex-1 p-6">
-					<a rel="noopener noreferrer" href="#" aria-label="Te nulla oportere reprimique his dolorum"></a>
-					<a rel="noopener noreferrer" href="#" className="text-xs tracki uppercase hover:underline dark:text-violet-400">Convenire</a>
-					<h3 className="flex-1 py-2 text-lg font-semibold leadi">Te nulla oportere reprimique his dolorum</h3>
-					<div className="flex flex-wrap justify-between pt-3 space-x-2 text-xs dark:text-gray-400">
-						<span>June 4, 2020</span>
-						<span>2.4K views</span>
-					</div>
-				</div>
-			</article>
 
-      <article className="flex flex-col dark:bg-gray-900">
-				<a rel="noopener noreferrer" href="#" aria-label="Te nulla oportere reprimique his dolorum">
-					<img alt="" className="object-cover w-full h-52 dark:bg-gray-500" src="https://source.unsplash.com/200x200/?fashion?4" />
-				</a>
-				<div className="flex flex-col flex-1 p-6">
-					<a rel="noopener noreferrer" href="#" aria-label="Te nulla oportere reprimique his dolorum"></a>
-					<a rel="noopener noreferrer" href="#" className="text-xs tracki uppercase hover:underline dark:text-violet-400">Convenire</a>
-					<h3 className="flex-1 py-2 text-lg font-semibold leadi">Te nulla oportere reprimique his dolorum</h3>
-					<div className="flex flex-wrap justify-between pt-3 space-x-2 text-xs dark:text-gray-400">
-						<span>June 4, 2020</span>
-						<span>2.4K views</span>
-					</div>
-				</div>
-			</article>
+
 		</div>
 	</div>
 </section>
